ce60064827b512a29b314f024127da7353a25119
Kiian <kp.stallinger@gmail.com>
2020-08-11 15:13:11 +0200

    ✅ Purge and minify in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,22 @@
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
 
 module.exports = {
-	plugins: [
-		require('postcss-import')(),
-		require('tailwindcss')(),
-		require('postcss-preset-env')({
-			// Full list of features: https://github.com/csstools/postcss-preset-env/blob/master/src/lib/plugins-by-id.js#L36
-			features: {
-				'nesting-rules': true, // delete if you don’t want nesting (optional)
-			},
-		}),
-			require('cssnano')({
-				preset: ['default', { discardComments: { removeAll: true } }],
-      }),
-       require('@fullhuman/postcss-purgecss')({
-        content: ['./**/*.html', './**/*.svelte'],
-        defaultExtractor: content =>
-          content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      })
-	],
-}
+  plugins: [
+    require("postcss-import")(),
+    require("tailwindcss")(),
+    require("postcss-preset-env")({
+      // Full list of features: https://github.com/csstools/postcss-preset-env/blob/master/src/lib/plugins-by-id.js#L36
+      features: {
+        "nesting-rules": true, // delete if you don’t want nesting (optional)
+      },
+    }),
+    !dev && require("cssnano")({
+      preset: ["default", { discardComments: { removeAll: true } }],
+    }),
+    !dev && require("@fullhuman/postcss-purgecss")({
+      content: ["./**/*.html", "./**/*.svelte"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
+};
